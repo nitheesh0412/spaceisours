3c1e62168b97c0b463deccb124356ddb34afda09
nitheesh0412 <nitheeshtangellapally4@gmail.com>
2022-09-23 14:34:47 +0530

    app created

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+import {Routes,Route,Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+import Home from "./components/Home";
+import Help from "./components/Help";
+import About from "./components/About";
+function App() {
+  return (
+    <div>
+      <div>
+        <h1 class="style">Can opener</h1>
+        <>
+        <nav className="navbar navbar-expand-lg mx-auto">
+    <div className="container-fluid ">
+     
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav ms-auto ">
+          <li className="nav-item me-3">
+            <Link className="nav-link active text-info"to="Home">Home</Link>
+          </li>
+          <li className="nav-item me-3">
+            <Link className="nav-link text-info" to="help">Help</Link>
+          </li>
+          <li className="nav-item me-3 ">
+            <Link className="nav-link text-info" to="aboutus">Aboutus</Link>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </nav>
+      </>
+      <Routes>
+            <Route path="/" element={< Home />} />
+            <Route path="/Home" element={< Home />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/aboutus" element={<About />} />
+          </Routes>
+          </div>
+
+
+    </div>
+  );
+}
+
+export default App;
