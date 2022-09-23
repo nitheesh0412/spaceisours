ed1253b27b274f401978409496cd28c3400cd60c
nitheesh0412 <nitheeshtangellapally4@gmail.com>
2022-09-23 15:20:04 +0530

    modified home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,83 @@ import React from "react"
 import Carousel from 'react-bootstrap/Carousel';
 function Home(){
     return(
-        <div>
+        <div className=""><Carousel>
+            <Carousel.Item>
+            <div className="row ms-5  me-5 mt-3 mb-3">
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTACYh5QCEYF5woWgr7RGMyUUChQ1-QKw0A5g&usqp=CAU"
+          alt="First slide"
+        />
+              </div>
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://d2pn8kiwq2w21t.cloudfront.net/images/imagesearthearthquake20191017ridgecrest201910.width-1024.jpg"
+          alt="First slide"
+        />
+              </div>
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBd7GY_6D04D38wVbPVtnrs616tosFpOEfJg&usqp=CAU"
+          alt="First slide"
+        />
+              </div>
+          </div>
+            </Carousel.Item>
+            <Carousel.Item>
+            <div className="row ms-5  me-5 mt-3 mb-3">
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDOXgMlE3_mSRe_e9vlbeWYvHqwmafO6tYw&usqp=CAU"
+          alt="First slide"
+        />
+              </div>
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://apod.nasa.gov/apod/image/1309/fires_mccolgan_960.jpg"
+          alt="First slide"
+        />
+              </div>
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://media.istockphoto.com/photos/eruption-in-iceland-picture-id1310008238?b=1&k=20&m=1310008238&s=170667a&w=0&h=EuRK5dtQ9wY5z4KBi-5aMi6znsIBTD6njzvn-78WYwY="
+          alt="First slide"
+        />
+              </div>
+          </div>
+            </Carousel.Item>
+            <Carousel.Item>
+            <div className="row ms-5  me-5 mt-3 mb-3">
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://media.istockphoto.com/photos/aerial-view-of-flooded-houses-with-dirty-water-of-dnister-river-in-picture-id1327617934?b=1&k=20&m=1327617934&s=170667a&w=0&h=aZi48tcwAUl8WkkmbVJY_kod0cnTHQUybRPn6JaZmro="
+          alt="First slide"
+        />
+              </div>
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://images.unsplash.com/photo-1604275689235-fdc521556c16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvb2R8ZW58MHx8MHx8&w=1000&q=80"
+          alt="First slide"
+        />
+              </div>
+              <div className="col-md-4">
+              <img class='st'
+          className=" w-100 h-100 image-center"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWmYI2UL0w-eLbkMt2CK7bTXqRdo-0pilFSw&usqp=CAU"
+          alt="First slide"
+        />
+              </div>
+          </div>
+            </Carousel.Item>
+{/*           
             <Carousel className="container mb-5 mt-3 ">
       <Carousel.Item>
         <img class='st'
@@ -68,7 +144,7 @@ function Home(){
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
-      </Carousel.Item>
+      </Carousel.Item> */}
     </Carousel>
         </div>
     )
